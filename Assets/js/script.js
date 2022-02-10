9daa532ea8752451ba0331cbd277bd09f1d90c22
AJB <designbyamber1@gmail.com>
2022-02-10 08:28:17 -0500

    created function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,19 @@
-// Assignment code here
+//create a function called generate password
+function generatePassword(){
+  //create alert “how many characters would you like your password to contain?(minimum 8, maximum 128)”
+  window.prompt("How many characters would you like your password to contain? (Minimum characters is 8, maximum characters is 128)");
+  
+  //if the value is less than 8 characters alert “password length must be at least 8 characters”
+  if (i < 8) {
+   window.alert("Please provide more than 8 characters");
+   return "Please provide more than 8 characters";
+  }
+}
+
+
+
+
+
 
 
 // Get references to the #generate element
