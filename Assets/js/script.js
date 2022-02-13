238e6fe4255176c8aff8000090d296850bd6bb5c
AJB <designbyamber1@gmail.com>
2022-02-13 13:02:25 -0500

    updated

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,31 +2,29 @@
 function generatePassword() {
   //create an alert "how many characters would you like your password to contain?"
   var charLength = window.prompt("How many characters would you like your password to contain?(minimum 8, maximum 128)");
-  console.log(charLength); 
+  console.log(charLength);
   //if the user input is less than or equal to 7 OR more than or equal to 129 then alert "Please enter a number between 8-128"
-  if(charLength <= 7 || charLength >= 129) {
+  if (charLength <= 7 || charLength >= 129 || isNaN(charLength)) {
     window.alert("Please enter a number between 8-128");
     return undefined;
   }
-  // //if the user input is a letter then alert "Please enter a number between 8-128"
-  // else if(Number.isNaN) {
-  //   window.alert("Please enter a number between 8-128");
-  //   return undefined;
-  // }
-   //if value is between 8-128, then prompt "click ok to confirm including special characters"
-   if(charLength >= 8 && charLength <=128) {
+  //if value is between 8-128, then prompt "click ok to confirm including special characters"
+  if (charLength >= 8 && charLength <= 128) {
     var specialChar = window.confirm("Click 'ok' to confirm including special characters");
     var numericChar = window.confirm("Click 'ok' to confirm including numerical characters")
     var lowercaseChar = window.confirm("Click 'ok' to confirm including lowercase characters");
     var uppercaseChar = window.confirm("Click 'ok' to confirm including uppercase characters");
-   }
+  }
 
-   
-   //generate password length based on charLength value
-   
-   //if specialChar is true, then include in password
 
-   //if specialChar is false, do not include in password
+  //length of password should equal length of charLength
+  password.length = charLength; 
+
+  //if specialChar is true, then include in password
+  if(specialChar){
+   //include in password
+  }
+//if specialChar is false, do not include in password
 }
 
 
@@ -42,6 +40,9 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+
+
 
   passwordText.value = password;
 
