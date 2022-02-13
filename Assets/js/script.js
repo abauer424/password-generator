acb5180537e950b7318e4c1e5e32b80616b4416c
AJB <designbyamber1@gmail.com>
2022-02-13 14:10:03 -0500

    done

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,15 @@
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+var lowerString = "abcdefghiqklmnopqrstuvwxyz"
+var lowerArray = lowerString.split("")
+var upperArray = lowerString.toUpperCase().split("")
+console.log(upperArray)
+var numArray = ["1" , "2","3" ,"4", "5", "6",]
+var specialArray = ["#" , "$","!" , "@" , "%", "^", "*", "(", ")"]
+
 //create a function called generate password
 function generatePassword() {
+  var totalArray = []
   //create an alert "how many characters would you like your password to contain?"
   var charLength = window.prompt("How many characters would you like your password to contain?(minimum 8, maximum 128)");
   console.log(charLength);
@@ -11,38 +21,42 @@ function generatePassword() {
   //if value is between 8-128, then prompt "click ok to confirm including special characters"
   if (charLength >= 8 && charLength <= 128) {
     var specialChar = window.confirm("Click 'ok' to confirm including special characters");
+    if(specialChar === true) {
+      totalArray = totalArray.concat(specialArray)
+    }
     var numericChar = window.confirm("Click 'ok' to confirm including numerical characters")
+    if(numericChar){
+      totalArray = totalArray.concat(numArray)
+    }
     var lowercaseChar = window.confirm("Click 'ok' to confirm including lowercase characters");
+    if(lowercaseChar) {
+      totalArray = totalArray.concat(lowerArray)
+    }
     var uppercaseChar = window.confirm("Click 'ok' to confirm including uppercase characters");
+    if(uppercaseChar) {
+      totalArray = totalArray.concat(upperArray)
+    }
+    if(totalArray.length === 0) {
+      window.alert("Please select one character type")
+      return "Please try again";
+    }
+  }
+  console.log(totalArray)
+var results = []
+  for(var i=0; i < charLength; i++){
+   var index = Math.floor(Math.random()*totalArray.length) 
+   console.log(index) 
+   var passDigit = totalArray[index]
+   results.push(passDigit)
   }
 
-
-  //length of password should equal length of charLength
-  password.length = charLength; 
-
-  //if specialChar is true, then include in password
-  if(specialChar){
-   //include in password
-  }
+return results.join("")
 //if specialChar is false, do not include in password
 }
-
-
-
-
-
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-
-
 
   passwordText.value = password;
 
@@ -51,4 +65,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-generatePassword();
