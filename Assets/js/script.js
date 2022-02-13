d8cdf2bcd89ab2943071f81a4168e49f9da4bcc0
AJB <designbyamber1@gmail.com>
2022-02-12 22:33:21 -0500

    updated

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,36 +1,25 @@
 //create a function called generate password
-function generatePassword(){
-  //create alert “how many characters would you like your password to contain?(minimum 8, maximum 128)”
-  let rawCharLength = window.prompt("How many characters would you like your password to contain? (Minimum characters is 8, maximum characters is 128)"); //[1]
-  let charLength = parseInt(rawCharLength); //[2]
-
-  //if the value is less than 8 characters alert 
-  if (charLength < 8) { //[3]
-   window.alert("Please provide more than 8 characters");
-   return undefined;
+function generatePassword() {
+  //create an alert "how many characters would you like your password to contain?"
+  var charLength = window.prompt("How many characters would you like your password to contain?(minimum 8, maximum 128)");
+  console.log(charLength); 
+  //if the user input is less than or equal to 7 OR more than or equal to 129 then alert "Please enter a number between 8-128"
+  if(charLength <= 7 || charLength >= 129) {
+    window.alert("Please enter a number between 8-128");
+    return undefined;
   }
- //if the value is more than 128 characters alert 
-  else if (charLength > 128) {//[4]
-  window.alert("Please provide less than 128 characters")
-  return undefined;
-  }
-  //if the value is a letter then alert "please enter a number between 8-128"
-  else if (Number.isNaN (charLength)) {
-  window.alert ("Please enter a number between 8-128")
-  return undefined;
-}
-
-  //if value is between 8-128, then prompt "click ok to confirm including special characters"
-  if (charLength >=8 && charLength <=128){
-    window.prompt("Click ok to confirm including special characters");
-
-    //if clicks "ok" then include in generate password
-
-    //if clicks "ok" then move onto next window prompt
-
-    //if clicks cancel then do not include in password 
-  }
-
+  // //if the user input is a letter then alert "Please enter a number between 8-128"
+  // else if(Number.isNaN) {
+  //   window.alert("Please enter a number between 8-128");
+  //   return undefined;
+  // }
+   //if value is between 8-128, then prompt "click ok to confirm including special characters"
+   if(charLength >= 8 && charLength <=128) {
+    var specialChar = window.confirm("Click 'ok' to confirm including special characters");
+    var numericChar = window.confirm("Click 'ok' to confirm including numerical characters")
+    var lowercaseChar = window.confirm("Click 'ok' to confirm including lowercase characters");
+    var uppercaseChar = window.confirm("Click 'ok' to confirm including uppercase characters");
+   }
 }
 
 
@@ -53,3 +42,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+generatePassword();
